f9949cc3239e6a84146b516431cf5c181ce26a49
Walter Rodrigo Munoz Zamorano <walter.munoz@neoris.cxnetworks.net>
2019-05-26 20:18:52 -0400

    primera carga

---
+++ superheroes/models.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    founder = models.CharField(max_length=100, blank=True, default='')
+
+    class Meta:
+        ordering = ('name',)
+
+
+class SuperHeroe(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    gender = models.CharField(max_length=100, blank=True, default='')
+    real_name = models.CharField(max_length=100, blank=True, default='')
+    publisher = models.ForeignKey(Publisher)
+
+    class Meta:
+        ordering = ('name',)
